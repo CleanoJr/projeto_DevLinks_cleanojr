2a317aa80db035d668f77c659f8132e8be9c09c2
CleanoJr <cleanojr@gmail.com>
2023-01-11 16:28:11 -0300

    ajustes no avatar

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ function toggleMode(){
     // Substituir a imagem
     if (html.classList.contains('light')) {
         //Se estiver em Light Mode
-        img.setAttribute("src", "./assets/Ellipse.png")
-        img.setAttribute("alt", "Foto com óculos escuro")
+        img.setAttribute("src", "./assets/avatar-light-mode.png")
+        img.setAttribute("alt", "Foto de Cleano Júnior de costas com uma camisa dourada com uma praia ao fundo")
         
     } else {
         //Se estiver em Dark Mode
-        img.setAttribute("src", "./assets/Avatar.png")
+        img.setAttribute("src", "./assets/avatar-dark-mode.png")
     }
 }
